6bf1236e3ed6b4906d11537ee7d947f1f6fc8d47
Master <masterdev41@gmail.com>
2025-07-13 09:16:29 +0300

    Query changes

--- python-decorators-0x01/0-log_queries.py
+++ python-decorators-0x01/0-log_queries.py
@@ -1,27 +1,29 @@
+from datetime import datetime
 import sqlite3
 import functools
 
 
 def log_queries():
-   def decorator_log_queries(func):
-       @functools.wraps(func)
-       def wrapper(*args, **kwargs):
-           query = kwargs.get('query') or (args[0] if args else None)
-           if query:
-               print(f"[LOG] Executing SQL Query: {query}")
-           return func(*args, **kwargs)
-       return wrapper
-   return decorator_log_queries
+    def decorator_log_queries(func):
+        @functools.wraps(func)
+        def wrapper(*args, **kwargs):
+            query = kwargs.get('query') or (args[0] if args else None)
+            if query:
+                timestamp = datetime.now().strftime("%Y-%m-%d %H:%M:%S")
+                print(f"[{timestamp}] Executing SQL Query: {query}")
+            return func(*args, **kwargs)
+        return wrapper
+    return decorator_log_queries
 
 
 @log_queries()
 def fetch_all_users(query):
-   conn = sqlite3.connect('users.db')
-   cursor = conn.cursor()
-   cursor.execute(query)
-   results = cursor.fetchall()
-   conn.close()
-   return results
+    conn = sqlite3.connect('users.db')
+    cursor = conn.cursor()
+    cursor.execute(query)
+    results = cursor.fetchall()
+    conn.close()
+    return results
 
 
 users = fetch_all_users(query="SELECT * FROM users")
