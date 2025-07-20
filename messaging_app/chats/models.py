ff75c0e71c91f7e05e2085a806956ca31019e3c0
Master <masterdev41@gmail.com>
2025-07-20 17:33:28 +0300

    Add User ID

--- messaging_app/chats/models.py
+++ messaging_app/chats/models.py
@@ -13,7 +13,7 @@ class User(AbstractUser):
         ('admin', 'Admin'),
     ]
     
-    id = models.UUIDField(
+    user_id = models.UUIDField(
         primary_key=True,
         default=uuid.uuid4,
         editable=False,
