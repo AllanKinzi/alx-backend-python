ead9f83f0934f16218391c1897452870acf65f56
Master <masterdev41@gmail.com>
2025-07-27 12:35:09 +0300

    Add Pagination

--- messaging_app/chats/views.py
+++ messaging_app/chats/views.py
@@ -6,6 +6,9 @@ from rest_framework.permissions import IsAuthenticated
 from django.shortcuts import get_object_or_404
 from rest_framework import status
 from rest_framework.response import Response
+from .pagination import StandardResultsSetPagination
+from .filters import MessageFilter
+from django_filters.rest_framework import DjangoFilterBackend
 
 class ConversationViewSet(viewsets.ModelViewSet):
     queryset = Conversation.objects.all()
@@ -19,16 +22,19 @@ class ConversationViewSet(viewsets.ModelViewSet):
 class MessageViewSet(viewsets.ModelViewSet):
     serializer_class = MessageSerializer
     permission_classes = [IsAuthenticated, IsParticipantOfConversation]
+    filter_backends = [DjangoFilterBackend]
+    filterset_class = MessageFilter
+    pagination_class = StandardResultsSetPagination
 
     def get_queryset(self):
         conversation_id = self.kwargs.get('conversation_id')
-        return Message.objects.filter(conversation__id=conversation_id, conversation__participants=self.request.user)
+        return Message.objects.filter(
+            conversation__id=conversation_id,
+            conversation__participants=self.request.user
+        )
 
     def perform_create(self, serializer):
         conversation_id = self.kwargs.get('conversation_id')
         conversation = get_object_or_404(Conversation, id=conversation_id)
-
-        if self.request.user not in conversation.participants.all():
-            return Response({'detail': 'Forbidden'}, status=status.HTTP_403_FORBIDDEN)
-
         serializer.save(sender=self.request.user, conversation=conversation)
+
--- messaging_app/messaging_app/settings.py
+++ messaging_app/messaging_app/settings.py
@@ -86,7 +86,7 @@ REST_FRAMEWORK = {
     ],
     
     # Pagination
-    'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.PageNumberPagination',
+    'DEFAULT_PAGINATION_CLASS': 'chats.pagination.StandardResultsSetPagination',
     'PAGE_SIZE': 20,
     
     # Filter backends
